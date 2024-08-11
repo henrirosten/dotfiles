eba6cd2483a7085cc4a0b2b8874ea130568f1cb6
Henri Rosten <henri.rosten@unikie.com>
2024-08-11 10:50:04 +0300

    Battery life improvements
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- nix-modules/laptop.nix
+++ nix-modules/laptop.nix
@@ -12,10 +12,25 @@
     };
   };
 
-  # battery life improvements
+  # Battery life improvements
+
   powerManagement.enable = true;
   # powerManagement.powertop.enable = true;
 
   # conflicts with services.power-profiles-daemon.enable enabled by Gnome:
   # services.tlp.enable = true;
+
+  # https://www.reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/
+  services.pipewire = {
+    wireplumber.extraConfig."10-disable-camera.conf" = {
+      "wireplumber.profiles".main."monitor.libcamera" = "disabled";
+    };
+  };
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = false;
+    };
+  };
 }
