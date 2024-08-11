ae0d3effc1d65c6c37505f12562b0b71de749b51
Henri Rosten <henri.rosten@unikie.com>
2024-08-11 09:53:39 +0300

    Flake update
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
@@ -11,7 +10,6 @@
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
     home-manager = {
-      # url = "github:nix-community/home-manager/release-23.11";
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
