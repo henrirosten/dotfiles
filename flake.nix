b315202669e503aa465751f7af5aea9e09fef225
Henri Rosten <henri.rosten@unikie.com>
2025-11-05 14:06:03 +0200

    Compare bombon and sbomnix meta extact
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- flake.nix
+++ flake.nix
@@ -23,10 +23,43 @@
       url = "github:Mic92/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    bombon.url = "github:nikstur/bombon";
   };
 
   outputs = inputs @ {self, ...}: let
     inherit (self) outputs;
+
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+
+    compare-runtime-meta = pkgs.writeShellScriptBin "run" ''
+      set -u
+      ${pkgs.lib.getExe pkgs.nix} build ${self.outPath}#sbom-bombon-x1-runtime --out-link tmp_bombon_runtime_out
+      echo "\"name\",\"version\",\"meta_description\"" >tmp_bombon_runtime.csv
+      ${pkgs.lib.getExe pkgs.jq} --raw-output '.components[] | [.name, .version, .description] | @csv' tmp_bombon_runtime_out >>tmp_bombon_runtime.csv
+      ${pkgs.csvkit}/bin/csvsql --query "select distinct name,version,meta_description from tmp_bombon_runtime where meta_description is not null order by name,version" tmp_bombon_runtime.csv >tmp_bombon_runtime_meta.csv
+      bombon_pkgs=$(${pkgs.csvkit}/bin/csvstat --count tmp_bombon_runtime.csv)
+      bombon_meta_pkgs=$(${pkgs.csvkit}/bin/csvstat --count tmp_bombon_runtime_meta.csv)
+
+      ${pkgs.lib.getExe pkgs.sbomnix} ${self.outPath}#nixosConfigurations.x1.config.system.build.toplevel --csv tmp_sbomnix_runtime.csv
+      ${pkgs.csvkit}/bin/csvsql --query "select distinct pname_meta,version_meta,meta_description from tmp_sbomnix_runtime where meta_description is not null order by pname_meta,version_meta" tmp_sbomnix_runtime.csv >tmp_sbomnix_runtime_meta.csv
+      sbomnix_pkgs=$(${pkgs.csvkit}/bin/csvstat --count tmp_sbomnix_runtime.csv)
+      sbomnix_meta_pkgs=$(${pkgs.csvkit}/bin/csvstat --count tmp_sbomnix_runtime_meta.csv)
+
+      ${pkgs.lib.getExe pkgs.nix} run github:tiiuae/ci-public/317d464?dir=csvdiff#csvdiff -- --cols=meta_description --ignoredups tmp_sbomnix_runtime.csv tmp_bombon_runtime.csv --out tmp_csvdiff.csv
+
+      echo ""
+      echo "[+] sbomnix missing nix meta from following packages: "
+      ${pkgs.csvkit}/bin/csvsql --query "select distinct name,diff from tmp_csvdiff where diff like 'right_only'" tmp_csvdiff.csv | cut -d"," -f1 | tail -n +2
+
+      echo ""
+      echo "[+] bombon missing nix meta from following packages: "
+      ${pkgs.csvkit}/bin/csvsql --query "select distinct name,diff from tmp_csvdiff where diff like 'left_only'" tmp_csvdiff.csv | cut -d"," -f1 | tail -n +2
+
+      echo ""
+      echo "[+] bombon:  packages with nix meta: $bombon_meta_pkgs (out of $bombon_pkgs packages)"
+      echo "[+] sbomnix: packages with nix meta: $sbomnix_meta_pkgs (out of $sbomnix_pkgs packages)"
+    '';
 
     specialArgs = {
       inherit inputs outputs;
@@ -40,9 +73,22 @@
         ];
       };
     };
-  in {
+  in rec {
     nixosModules = import ./nix-modules;
     homeManagerModules = import ./home-modules;
+
+    apps.x86_64-linux = {
+      run-compare-runtime-meta = {
+        type = "app";
+        program = "${compare-runtime-meta}/bin/run";
+      };
+    };
+
+    packages.x86_64-linux = {
+      sbom-bombon-x1-runtime = inputs.bombon.lib.x86_64-linux.buildBom nixosConfigurations.x1.config.system.build.toplevel {
+        includeBuildtimeDependencies = false;
+      };
+    };
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager switch --flake .#hrosten'
